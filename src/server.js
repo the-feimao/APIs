c350da5624e2d32acff694c26077668c93b7795d
the-feimao <thefeimao.ng@gmail.com>
2026-05-19 12:05:09 +0100

    changed from npm to pnpm

--- src/server.js
+++ src/server.js
@@ -1,10 +1,16 @@
 import 'dotenv/config';
 import express from 'express';
+import cors from 'cors';
 
 //import routes
 import authRoutes from './routes/auth.routes.js';
 import taskRoutes from './routes/task.routes.js';
 const app = express();
+
+app.use(cors({
+  origin: process.env.CORS_ORIGIN || 'http://localhost:3000',
+  credentials: true
+}));
 
 //middlewares
 app.use(express.json());
@@ -12,7 +18,7 @@ app.use(express.json());
 //API routes
 app.use('/api/auth', authRoutes);
 app.use('/api/tasks', taskRoutes);
-app.get('/', (req, res) => {
+app.get('/api', (req, res) => {
   res.send('Hello, World!');
 });
 const PORT = process.env.PORT || 5001;
